6b0f47b93463dcc8ab7f4655ac2b5658e7db85e4
FabiodAgostino <83577536+FabiodAgostino@users.noreply.github.com>
2022-12-07 10:14:17 +0100

    fsd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBNWbqdAaq44dWHELm2m1jU0xY6graA9uo",
+    authDomain: "rotiniel-35c5b.firebaseapp.com",
+    projectId: "rotiniel-35c5b",
+    storageBucket: "rotiniel-35c5b.appspot.com",
+    messagingSenderId: "546264380520",
+    appId: "1:546264380520:web:b09f4bc5e62c611818f7dd",
+    measurementId: "G-VXQ0LYQVDB"
+  }
 };
