7c7182bf589f6ac0cc86701290ecd7fee84b68f3
Fabio D'Agostino <fabio1994dagostino@gmail.com>
2025-02-26 13:50:55 +0100

    test variabili d'ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,23 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   firebaseConfig: {
+//     apiKey: "AIzaSyBNWbqdAaq44dWHELm2m1jU0xY6graA9uo",
+//     authDomain: "rotiniel-35c5b.firebaseapp.com",
+//     projectId: "rotiniel-35c5b",
+//     storageBucket: "rotiniel-35c5b.appspot.com",
+//     messagingSenderId: "546264380520",
+//     appId: "1:546264380520:web:b09f4bc5e62c611818f7dd",
+//     measurementId: "G-VXQ0LYQVDB"
+//   }
+// };
+
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBNWbqdAaq44dWHELm2m1jU0xY6graA9uo",
-    authDomain: "rotiniel-35c5b.firebaseapp.com",
-    projectId: "rotiniel-35c5b",
-    storageBucket: "rotiniel-35c5b.appspot.com",
-    messagingSenderId: "546264380520",
-    appId: "1:546264380520:web:b09f4bc5e62c611818f7dd",
-    measurementId: "G-VXQ0LYQVDB"
+    
   }
 };
 
