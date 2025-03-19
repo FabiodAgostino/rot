b24ee58c580086eda82deb3e2072cace7395b974
Fabio D'Agostino <fabio1994dagostino@gmail.com>
2025-03-19 10:15:25 +0100

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,26 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
   discordConfig:{
+    redirectUrl:'',
+    clientId:'',
+    clientSecret : '',
+    guildId:'',
+    loginDiscord:""
+  },
+  discordConfigLocal:{
+    redirectUrl:'',
+    clientId:'',
+    clientSecret : '',
+    guildId:'',
+    loginDiscord:""
   }
 };
