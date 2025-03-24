776926ab343052b1df51c2abde197bf70671d49e
Fabio D'Agostino <fabio1994dagostino@gmail.com>
2025-03-24 17:49:29 +0100

    d

--- src/environments/environment.prod.template.ts
+++ src/environments/environment.prod.template.ts
@@ -10,17 +10,10 @@ export const environment = {
       measurementId: "${FIREBASE_MEASUREMENT_ID}"
     },
     discordConfig: {
-      redirectUrl: "https://fabiodagostino.github.io/rot/",
-      clientId: "1106594210242625579",
-      clientSecret: "${CLIENT_SECRET}",
-      guildId: "511856322141093904",
-      loginDiscord: "https://discord.com/api/oauth2/authorize?client_id=1106594210242625579&redirect_uri=https%3A%2F%2Ffabiodagostino.github.io%2Frot%2F&response_type=code&scope=identify%20guilds%20connections%20guilds.members.read"
+      redirectUrl: "${DISCORD_REDIRECT_URL}",
+      clientId: "${DISCORD_CLIENT_ID}",
+      clientSecret: "${DISCORD_CLIENT_SECRET}",
+      guildId: "${DISCORD_GUILD_ID}",
+      loginDiscord: "${DISCORD_LOGIN_DISCORD}"
     },
-    discordConfigLocal: {
-      redirectUrl: "http://localhost:4200/",
-      clientId: "1106594210242625579",
-      clientSecret: "${CLIENT_SECRET}",
-      guildId: "511856322141093904",
-      loginDiscord: "https://discord.com/api/oauth2/authorize?client_id=1106594210242625579&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code&scope=identify%20guilds%20connections%20guilds.members.read"
-    }
   };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,17 +10,17 @@ export const environment = {
     measurementId: ""
   },
   discordConfig:{
-    redirectUrl:'https://fabiodagostino.github.io/rot/',
-    clientId:'1106594210242625579',
-    clientSecret : 'vSpoEDdg9pho-4Fltzo736cgwyPQk2g1',
-    guildId:'511856322141093904',
-    loginDiscord:"https://discord.com/api/oauth2/authorize?client_id=1106594210242625579&redirect_uri=https%3A%2F%2Ffabiodagostino.github.io%2Frot%2F&response_type=code&scope=identify%20guilds%20connections%20guilds.members.read"
+    redirectUrl:'',
+    clientId:'',
+    clientSecret : '',
+    guildId:'',
+    loginDiscord:""
   },
   discordConfigLocal:{
-    redirectUrl:'http://localhost:4200/',
-    clientId:'1106594210242625579',
-    clientSecret : 'vSpoEDdg9pho-4Fltzo736cgwyPQk2g1',
-    guildId:'511856322141093904',
-    loginDiscord:"https://discord.com/api/oauth2/authorize?client_id=1106594210242625579&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code&scope=identify%20guilds%20connections%20guilds.members.read"
+    redirectUrl:'',
+    clientId:'',
+    clientSecret : '',
+    guildId:'',
+    loginDiscord:""
   }
 };
