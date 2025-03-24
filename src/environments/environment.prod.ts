d71e2092c45fd7968f6fc43f5ee4d8ce70bd01d3
Fabio D'Agostino <fabio1994dagostino@gmail.com>
2025-03-24 16:31:36 +0100

    d

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyBNWbqdAaq44dWHELm2m1jU0xY6graA9uo",
+    authDomain: "rotiniel-35c5b.firebaseapp.com",
+    projectId: "rotiniel-35c5b",
+    storageBucket: "rotiniel-35c5b.appspot.com",
+    messagingSenderId: "546264380520",
+    appId: "1:546264380520:web:b09f4bc5e62c611818f7dd",
+    measurementId: "G-VXQ0LYQVDB"
   },
   discordConfig:{
     redirectUrl:'https://fabiodagostino.github.io/rot/',
