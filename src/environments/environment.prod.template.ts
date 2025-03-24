577d5221bc3d63836e26b49e7d3cd67416f15f8e
Fabio D'Agostino <fabio1994dagostino@gmail.com>
2025-03-24 17:54:02 +0100

    d

--- src/environments/environment.prod.template.ts
+++ src/environments/environment.prod.template.ts
@@ -16,4 +16,11 @@ export const environment = {
       guildId: "${DISCORD_GUILD_ID}",
       loginDiscord: "${DISCORD_LOGIN_DISCORD}"
     },
+    discordConfigLocal: { 
+        redirectUrl: "http://localhost:4200/",
+        clientId: "${DISCORD_CLIENT_ID}",
+        clientSecret: "${DISCORD_CLIENT_SECRET}",
+        guildId: "${DISCORD_GUILD_ID}",
+        loginDiscord: "https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code&scope=identify%20guilds%20connections%20guilds.members.read"
+      }
   };
